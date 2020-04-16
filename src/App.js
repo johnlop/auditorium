a18db005c594f199524597a92c7b528a70c0956d
Jon <jon.l@agencywithin.com>
2020-04-16 18:32:14 -0400

    Jutsu

--- src/App.js
+++ src/App.js
@@ -1,36 +1,52 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import JitsiMeetJS from "lib-jitsi-meet-dist";
+import React, { useState } from "react";
+import { Jutsu } from "react-jutsu";
 
-function App() {
-  JitsiMeetJS.init();
-  var connection = new JitsiMeetJS.JitsiConnection(null, null, {
-    serviceUrl: "ererer",
-  });
+const App = () => {
+  const [room, setRoom] = useState("");
+  const [name, setName] = useState("");
+  const [call, setCall] = useState(false);
+  const [password, setPassword] = useState("");
 
-  const onConnectionSuccess = () => {
-    console.log("CONNECTION_ESTABLISHED");
-    const room = connection.initJitsiConference("conference1", {});
-
-    room.join();
+  const handleClick = (event) => {
+    event.preventDefault();
+    if (room && name) setCall(true);
   };
 
-  connection.addEventListener(
-    JitsiMeetJS.events.connection.CONNECTION_ESTABLISHED,
-    onConnectionSuccess
+  return call ? (
+    <Jutsu
+      roomName={room}
+      userName={name}
+      password={password}
+      loadingComponent={<p>loading ...</p>}
+    />
+  ) : (
+    <form>
+      <input
+        id="room"
+        type="text"
+        placeholder="Room"
+        value={room}
+        onChange={(e) => setRoom(e.target.value)}
+      />
+      <input
+        id="name"
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        id="password"
+        type="text"
+        placeholder="Password (optional)"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleClick} type="submit">
+        Start / Join
+      </button>
+    </form>
   );
-
-  connection.connect();
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello!</p>
-      </header>
-    </div>
-  );
-}
+};
 
 export default App;
